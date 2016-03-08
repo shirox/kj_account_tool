ebf07c505006498f27d8ae9b24986cd1ef42d9a1
shirox <shirox.com@gmail.com>
2016-03-08 09:30:19 +0900

    Delete s3 configs.

--- conf/config.php
+++ conf/config.php
@@ -12,19 +12,4 @@ class Config {
 	     ];
       }
 
-      public static function s3Config() {
-      	     return [
-	     	    "version" => "",
-	     	    "region" => "",
-	     	    "credentials" => [
-		    		  "key" => "",
-		    		  "secret" => "",
-		    ],
-	     ];
-      }
-
-      public static function getS3BucketName() {
-      	     return "kojima";
-      }
-
 }
